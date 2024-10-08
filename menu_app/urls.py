087a6d79a62924da70eb9dce22d03af0eda3c950
MaksimEvdokimov <maksorli@gmail.com>
2024-10-08 23:39:34 +0400

    add second menu

--- menu_app/urls.py
+++ menu_app/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('punkt1/', views.punkt1_view, name='punkt1'),
+    path('punkt2/', views.punkt2_view, name='punkt2'),
     path('<slug:slug>/', views.menu_item_view, name='menu_item'),
 ]
--- menu_app/views.py
+++ menu_app/views.py
@@ -6,6 +6,8 @@ def home(request):
     return render(request, 'menu_app/home.html')
 def punkt1_view(request):
     return render(request, 'menu_app/punkt1.html')
+def punkt2_view(request):
+    return render(request, 'menu_app/punkt2.html')
 def menu_item_view(request, slug):
     menu_item = get_object_or_404(MenuItem, slug=slug)
     context = {
